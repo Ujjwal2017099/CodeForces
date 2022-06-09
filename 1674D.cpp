a83cb3d7018084c684088e94ad50580c15f8a2ec
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-06-09 21:37:50 +0530

    A-B-C Sort
    You are given three arrays a, b and c. Initially, array a consists of n elements, arrays b and c are empty.
    
    You are performing the following algorithm that consists of two steps:
    
    Step 1: while a is not empty, you take the last element from a and move it in the middle of array b. If b currently has odd length, you can choose: place the element from a to the left or to the right of the middle element of b. As a result, a becomes empty and b consists of n elements.
    Step 2: while b is not empty, you take the middle element from b and move it to the end of array c. If b currently has even length, you can choose which of two middle elements to take. As a result, b becomes empty and c now consists of n elements.
    Refer to the Note section for examples.
    Can you make array c sorted in non-decreasing order?

---
+++ 1674D.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define int long long
+#define float double
+#define vi vector<int>
+#define vf vector<float>
+#define vb vector<bool>
+#define vc vector<char>
+#define vs vector<string>
+#define rep(i,a,b) for(int i = a;i<=b;i++)
+#define rrep(i,a,b) for(int i = a;i>=b;i--)
+#define jaldi ios_base::sync_with_stdio(false);
+#define kar cin.tie(NULL);
+#define bhai cout.tie(NULL);
+#define no "NO"
+#define yes "YES"
+#define all(sexy69) sexy69.begin(),sexy69.end()
+using namespace std;
+
+template <typename T>
+istream &operator>>(istream &istream, vector<T> &v)
+{
+     for (auto &it : v)
+          cin >> it;
+     return istream;
+}
+
+template <typename Te>
+ostream &operator<<(ostream &ostream, vector<Te> &v)
+{
+     for (auto it : v)
+     {
+          cout << it << " ";
+     }
+     cout << endl;
+     return ostream;
+}
+void solution(){
+     int n;
+     cin>>n;
+     vi v(n);cin>>v;
+     reverse(all(v));
+     // cout<<v;
+     rep(i,0,n-2){
+          if(v[i] < v[i+1]) swap(v[i],v[i+1]);
+          i++;
+     }
+     reverse(all(v));
+     if(is_sorted(all(v))){cout<<yes<<endl;}
+     else cout<<no<<endl;
+
+}
+signed main()
+{
+     jaldi kar bhai
+     int t;
+     cin >> t;
+
+     while (t--)
+     {
+
+          solution();
+     }
+}
