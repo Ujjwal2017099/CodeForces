af8fd2f46fbad86bdc4b0a16ef795fed851f750f
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-07-16 14:42:54 +0530

    Paranoid String
    Let's call a binary string T of length m indexed from 1 to m paranoid if we can obtain a string of length 1 by performing the following two kinds of operations m−1 times in any order :
    
    Select any substring of T that is equal to 01, and then replace it with 1.
    Select any substring of T that is equal to 10, and then replace it with 0.
    For example, if T= 001, we can select the substring [T2T3] and perform the first operation. So we obtain T= 01.
    
    You are given a binary string S of length n indexed from 1 to n. Find the number of pairs of integers (l,r) 1≤l≤r≤n such that S[l…r] (the substring of S from l to r) is a paranoid string.
    
    https://codeforces.com/problemset/problem/1694/B

---
+++ 1694B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define int long long
+#define float double
+#define vi vector<int>
+#define vf vector<float>
+#define vb vector<bool>
+#define vc vector<char>
+#define vs vector<string>
+#define rep(i,a,b) for(int i = a;i<=b;i++)
+#define rrep(i,a,b) for(int i = a;i>=b;i--)
+#define jaldi ios_base::sync_with_stdio(false);
+#define kar cin.tie(NULL);
+#define bhai cout.tie(NULL);
+#define no "NO"
+#define yes "YES"
+#define all(sexy69) sexy69.begin(),sexy69.end()
+#define endl " \n"
+using namespace std;
+
+template <typename T>
+istream &operator>>(istream &istream, vector<T> &v)
+{
+     for (auto &it : v)
+          cin >> it;
+     return istream;
+}
+
+template <typename Te>
+ostream &operator<<(ostream &ostream, vector<Te> &v)
+{
+     for (auto it : v)
+     {
+          cout << it << " ";
+     }
+     cout << endl;
+     return ostream;
+}
+void solution(){
+     int n;cin>>n;
+     string s;
+     cin>>s;
+
+     int cnt = n;
+     rep(i,0,n-1){
+          if(i+1<n and s[i] != s[i+1]) cnt += (i+1);
+     }
+     cout<<cnt<<endl;
+}
+signed main()
+{
+     jaldi kar bhai
+     int t;
+     cin >> t;
+
+     while (t--)
+     {
+
+          solution();
+     }
+}
