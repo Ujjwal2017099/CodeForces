64b3fa6ea888f6442287e3eee2c78b0dc4be367c
Ujjwal2017099 <100745680+Ujjwal2017099@users.noreply.github.com>
2022-07-18 14:00:14 +0530

    New Year Transportation
    New Year is coming in Line World! In this world, there are n cells numbered by integers from 1 to n, as a 1 × n board. People live in cells. However, it was hard to move between distinct cells, because of the difficulty of escaping the cell. People wanted to meet people who live in other cells.
    
    So, user tncks0121 has made a transportation system to move between these cells, to celebrate the New Year. First, he thought of n - 1 positive integers a1, a2, ..., an - 1. For every integer i where 1 ≤ i ≤ n - 1 the condition 1 ≤ ai ≤ n - i holds. Next, he made n - 1 portals, numbered by integers from 1 to n - 1. The i-th (1 ≤ i ≤ n - 1) portal connects cell i and cell (i + ai), and one can travel from cell i to cell (i + ai) using the i-th portal. Unfortunately, one cannot use the portal backwards, which means one cannot move from cell (i + ai) to cell i using the i-th portal. It is easy to see that because of condition 1 ≤ ai ≤ n - i one can't leave the Line World using portals.
    
    Currently, I am standing at cell 1, and I want to go to cell t. However, I don't know whether it is possible to go there. Please determine whether I can go to cell t by only using the construted transportation system.
    
    https://codeforces.com/problemset/problem/500/A

---
+++ 500A.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define int long long
+#define float double
+#define vi vector<int>
+#define vf vector<float>
+#define vb vector<bool>
+#define vc vector<char>
+#define vs vector<string>
+#define rep(i,a,b) for(int i = a;i<=b;i++)
+#define rrep(i,a,b) for(int i = a;i>=b;i--)
+#define jaldi ios_base::sync_with_stdio(false);
+#define kar cin.tie(NULL);
+#define bhai cout.tie(NULL);
+#define no "NO"
+#define yes "YES"
+#define all(sexy69) sexy69.begin(),sexy69.end()
+#define endl " \n"
+using namespace std;
+
+template <typename T>
+istream &operator>>(istream &istream, vector<T> &v)
+{
+     for (auto &it : v)
+          cin >> it;
+     return istream;
+}
+
+template <typename Te>
+ostream &operator<<(ostream &ostream, vector<Te> &v)
+{
+     for (auto it : v)
+     {
+          cout << it << " ";
+     }
+     cout << endl;
+     return ostream;
+}
+void solution(){
+     int n,t;cin>>n>>t;
+     n--;
+     vi v(n);cin>>v;
+     v.push_back(0);
+     rep(i,0,n){
+          if(i+1 == t){
+               cout<<yes;
+               return;
+          }
+          if(i+1 > t) break;
+          i = i+v[i] -1 ;
+     }
+
+     cout<<no;
+}
+signed main()
+{
+     jaldi kar bhai
+     int t=1;
+     // cin >> t;
+
+     while (t--)
+     {
+
+          solution();
+     }
+}
